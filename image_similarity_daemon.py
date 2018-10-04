e9b8b21a96eaa3d168f43fbd520b236d447821ef
Yoonsik Jung <developer.kelvin@gmail.com>
2018-10-04 23:00:27 +0900

    ADD : DB Connection 객체를 클래스 내부 변수로 선언 ADD : 객체 소멸자에 db 커넥션 종료시키는 함수 추가 ADD : 유사도 검증 이후 이동하려는 경로에 폴더 없으면 생성하는 코드 추가 MOD : 파일 저장하는 경로 수정 BUG : similarity Numpy 타입에서 float 타입으로 변경

--- image_similarity_daemon.py
+++ image_similarity_daemon.py
@@ -4,42 +4,80 @@ import pymysql
 from env_setting import host, user, password, db
 import urllib.request
 import os
+import numpy
 
 
 class ImageValidator:
 
     def __init__(self, test_mode=False):
+
+        self.get_connection()
+
+        self.positive_img_count = 0
+        self.negative_img_count = 0
+        self.total_img_count = 0
         try:
-            conn = self.get_connection()
+            conn = self.conn
             self.test_mode = test_mode
 
             with conn.cursor() as cursor:
                 param_init_sql = 'SELECT positive_img_count, negative_img_count, total_img_count ' \
-                      'FROM similarity_param'
+                                 'FROM similarity_param'
                 cursor.execute(param_init_sql)
                 result = cursor.fetchone()
 
                 self.positive_img_count = result['positive_img_count']
                 self.negative_img_count = result['negative_img_count']
                 self.total_img_count = result['total_img_count']
-        finally:
-            conn.close()
+        except Exception as e:
+            print(e)
+        # finally:
+        #     conn.close()
 
     def get_connection(self):
-        conn = pymysql.connect(host=host,
-                               user=user,
-                               password=password,
-                               db=db,
-                               charset='utf8',
-                               cursorclass=pymysql.cursors.DictCursor)
+        is_conn_success = False
+        while not is_conn_success:
+            try:
+                self.conn = pymysql.connect(host=host,
+                                       user=user,
+                                       password=password,
+                                       db=db,
+                                       charset='utf8',
+                                       cursorclass=pymysql.cursors.DictCursor)
+            except:
+                print("db connection exception occures")
+                continue
 
-        return conn
+            if self.conn is not None:
+                is_conn_success = True
+
+        return self.conn
+
+    def disconnect_connection(self):
+        self.conn.close()
+
+    def __del__(self):
+        self.disconnect_connection()
+
+    def init_params(self):
+
+        try:
+            conn = self.conn
+            sql = 'UPDATE similarity_param SET positive_img_count = 0, negative_img_count = 0, total_img_count = 0'
+            with conn.cursor() as cursor:
+                cursor.execute(sql)
+
+        except:
+            print("init_params occurs exception")
+
+        # finally:
+        #     conn.close()
 
     # png일때랑 jpg 일때랑 고려할 것
     def download_img(self, url):
 
         # file path and file name to download
-        path = os.getcwd() + "/download"
+        path = os.getcwd() + "/download/"
 
         filename = "downloaded_img.jpg"
 
@@ -48,11 +86,27 @@ class ImageValidator:
             os.makedirs(path)
 
         # download
-        urllib.request.urlretrieve(url, path + filename)
+        is_download_success = False
+        try_count = 0
+        while not is_download_success:
+            try:
+                urllib.request.urlretrieve(url, path + filename)
+            except:
+                print("download failed. try again...")
+                continue
+
+            is_download_success = True
 
     def similarity_test(self, search_keyword=''):
         # 나중에 new 뺄것
-        target_img_path = 'download/new_target_img.jpg'
+        if search_keyword == '경복궁':
+            # target_img_path = 'reference/Kwanghwamun-reference.jpg'
+            target_img_path = 'download/new_target_img.jpg'
+        elif search_keyword == '창덕궁':
+            target_img_path = 'reference/Chandeokgung-Injeongjeon-reference.jpg'
+        else:
+            # for test
+            target_img_path = 'download/new_target_img.jpg'
         input_img_paths = 'download/downloaded_img.jpg'
 
         import time
@@ -85,15 +139,16 @@ class ImageValidator:
 
         try:
             # get db connection
-            connection = self.get_connection()
+            connection = self.conn
             # get download url from db
 
             with connection.cursor() as cursor:
                 # Create a new record
-                get_url_sql = 'SELECT image_url, search_keyword FROM image_info WHERE image_info.is_saved = 0 LIMIT 1'
+                get_url_sql = 'SELECT image_idx, image_url, search_keyword FROM image_info WHERE status = 0 LIMIT 1'
                 cursor.execute(get_url_sql)
                 res = cursor.fetchone()
                 url = res['image_url']
+                image_idx = res['image_idx']
                 search_keyword = res['search_keyword']
                 print('url : ', url)
                 print('search_keyword : ', search_keyword)
@@ -103,6 +158,8 @@ class ImageValidator:
 
             # validate img
             similarity = self.similarity_test()
+            if isinstance(similarity, numpy.generic):
+                similarity = numpy.asscalar(similarity)
             # threshold를 넘으면 1 안넘으면 2로 설정
             status = 0
             base_path = os.getcwd()
@@ -116,48 +173,59 @@ class ImageValidator:
                 base_positive_path = os.getcwd() + '/positive/'
                 img_path = str(self.positive_img_count // 1000) + '/'
                 img_name = str(self.positive_img_count % 1000) + '.jpg'
+                file_path = base_positive_path + img_path
+                file_address = base_positive_path + img_path + img_name
 
             else:
                 status = 2
                 # 이미지가 이동할 경로 설정(유사하지 않은 이미지 경로)
                 self.negative_img_count += 1
-                base_positive_path = os.getcwd() + '/negative/'
+                base_negative_path = os.getcwd() + '/negative/'
                 img_path = str(self.negative_img_count // 1000) + '/'
                 img_name = str(self.negative_img_count % 1000) + '.jpg'
-
-            file_address = base_path + img_path + img_name
+                file_path = base_negative_path + img_path
+                file_address = base_negative_path + img_path + img_name
 
             # 이미지를 file_address 로 이동시킴
             # 폴더 없으면 만드는 코드 작성(os,mkdir())
-            if not os.path.isdir(base_path+img_path):
-                os.makedirs(base_path+img_path)
 
-            os.rename(base_path + '/downloaded_img.jpg', file_address)
+            print('base path : ' + base_path + '\\downloaded_img.jpg')
+            print('path : ', file_address)
+
+            download_file_path = str(base_path + '\\download\\downloaded_img.jpg').replace("\\", '/')
+            after_move_path = file_address.replace("\\", '/')
+            # Create when directory does not exist
+            if not os.path.isdir(file_path):
+                os.makedirs(file_path)
+
+            os.rename(download_file_path, after_move_path)
 
             self.total_img_count += 1
 
             # update img
             with connection.cursor() as cursor:
-                insert_img_param_sql = 'UPDATE image_info SET status = %s, similarity = %s, file_address = %s'
-                cursor.execute(insert_img_param_sql, (status, similarity, file_address))
+                insert_img_param_sql = 'UPDATE image_info SET status = %s, similarity = %s, file_address = %s ' \
+                                       'WHERE image_idx = %s'
+                cursor.execute(insert_img_param_sql, (status, similarity, file_address, image_idx))
+                connection.commit()
 
         finally:
             # 현재까지의 이미지 사진들을 db에 저장함
 
-            counts = dict()
-            counts['positive_img_count'] = self.positive_img_count
-            counts['negative_img_count'] = self.negative_img_count
-            counts['total_img_count'] = self.total_img_count
-
             params = (self.positive_img_count, self.negative_img_count, self.total_img_count)
+            print('params' , params)
 
             with connection.cursor() as cursor:
-                insert_params_sql = 'INSERT INTO similarity_param(positive_img_count, negative_img_count, total_img_count)' \
-                                    ' value (%s, %s, %s)'
-                cursor.execute(insert_params_sql, params)
+                update_param_sql = 'UPDATE similarity_param SET positive_img_count = %s, negative_img_count = %s, total_img_count = %s'
+                # insert_params_sql = 'INSERT INTO similarity_param(positive_img_count, negative_img_count, total_img_count)' \
+                #                     ' value (%s, %s, %s)'
+                cursor.execute(update_param_sql, params)
+                connection.commit()
 
-            connection.close()
+            # connection.close()
 
 
 if __name__ == "__main__":
     obj = ImageValidator()
+    for i in range(100):
+        obj.validate_img(0.6)
